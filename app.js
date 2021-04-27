43897fc6196904885757c578d47e4cc8c07977f3
Marc Apesos <apesos2013@gmail.com>
2021-04-27 15:30:54 -0400

    first commit, mobile responsive

---
+++ app.js
@@ -0,0 +1,22 @@
+const d = document.querySelector("#days");
+const h = document.querySelector("#hours");
+const m = document.querySelector("#minutes");
+const s = document.querySelector("#seconds");
+
+function getTime() {
+  const currTime = new Date();
+  const bengalsWin = new Date("January 6, 1991 15:45:00");
+  const diff = currTime.getTime() - bengalsWin.getTime();
+  console.log(diff);
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+  console.log(days);
+  const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(diff / 1000 / 60) % 60;
+  const seconds = Math.floor(diff / 1000) % 60;
+  d.innerHTML = days;
+  h.innerHTML = hours;
+  m.innerHTML = minutes;
+  s.innerHTML = seconds;
+}
+
+setInterval(getTime, 1000);
